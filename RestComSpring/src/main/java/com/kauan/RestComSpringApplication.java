ad46bc318db84bb3256435878f75f43a204b76f6
Kauan <kauankenney16752@gmail.com>
2024-05-19 08:08:48 -0300

    Primeiro commit

---
+++ RestComSpring/src/main/java/com/kauan/RestComSpringApplication.java
@@ -0,0 +1,13 @@
+package com.kauan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestComSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestComSpringApplication.class, args);
+	}
+
+}
---
+++ RestComSpring/src/test/java/com/kauan/RestComSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kauan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestComSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
